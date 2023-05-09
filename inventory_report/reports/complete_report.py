8602ea09e74e2ded11980b75a0f0f906678eb1b7
Layane Nunes <layanenu@gmail.com>
2023-05-09 20:54:35 -0300

    feat: funcao para gerar a versão completa do relatório

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,13 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(data):
+        report = SimpleReport.generate(data)
+        report += "\nProdutos estocados por empresa:\n"
+        companies = Counter([item["nome_da_empresa"] for item in data])
+        for company, quantity in companies.items():
+            report += f"- {company}: {quantity}\n"
+        return report
