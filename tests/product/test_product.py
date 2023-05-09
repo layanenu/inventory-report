376c96e0893fee3676a130937e30f2be13ec472d
Layane Nunes <layanenu@gmail.com>
2023-05-09 12:08:42 -0300

    feat: teste para o construtor/inicializador do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        id=1,
+        nome_do_produto="Borracha",
+        nome_da_empresa="Papelaria Solar",
+        data_de_fabricacao="2021-07-04",
+        data_de_validade="2029-02-09",
+        numero_de_serie="FR48",
+        instrucoes_de_armazenamento="Ao abrigo de luz solar"
+    )
+
+    assert new_product.id == 1
+    assert new_product.nome_do_produto == "Borracha"
+    assert new_product.nome_da_empresa == "Papelaria Solar"
+    assert new_product.data_de_fabricacao == "2021-07-04"
+    assert new_product.data_de_validade == "2029-02-09"
+    assert new_product.numero_de_serie == "FR48"
+    assert new_product.instrucoes_de_armazenamento == "Ao abrigo de luz solar"
